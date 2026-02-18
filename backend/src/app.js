cf0f341562f36775dff14e4ef8ee9f05e6460209
Hawzz7 <vicky.007.cr@gmail.com>
2026-02-19 01:25:14 +0530

    added the backend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,8 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "https://lead-management-table.onrender.com"
   ],
   credentials: true
 }));
